b6719cfa2e13ba2e7c0553c45711b536c06f1ccb
kietnt <nguyentuankiet0105@gmail.com>
2023-03-20 00:02:20 +0700

    s initial

---
+++ constant.js
@@ -0,0 +1,22 @@
+export const dump = [
+ {
+  id: 1,
+  name: 'a',
+  age: 20
+ },
+ {
+  id: 2,
+  name: 'b',
+  age: 21
+ },
+ {
+  id: 3,
+  name: 'c',
+  age: 22
+ },
+ {
+  id: 4,
+  name: 'd',
+  age: 23
+ },
+]
